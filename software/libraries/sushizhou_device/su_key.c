b4c7010f4b6bda905e9e58580fa9e2c157a3a36b
MrSu18 <3051605203@qq.com>
2023-01-30 14:41:58 +0800

    上传独立按键的测试main函数

--- software/libraries/sushizhou_device/su_key.c
+++ software/libraries/sushizhou_device/su_key.c
@@ -27,3 +27,31 @@ uint8_t ReadKey(void)
     return key_value;
 }
 
+/****************************************∂¿¡¢∞¥º¸≤‚ ‘≥Ã–Ú***************************************
+void main(void)
+{
+	uint8_t temp;
+    ENABLE_LED_LATCH;
+	while (1)
+    {
+		temp=ReadKey();
+		if(temp!=0xff)
+		{
+			Delay10ms();
+			temp=ReadKey();
+			if(temp!=0xff)
+			{
+				switch(temp)
+				{
+					case 1: LED1=!LED1;break;
+					case 2: LED2=!LED2;break;
+					case 3: LED3=!LED3;break;
+					case 4: LED4=!LED4;break;
+					default: LED5=!LED5;break;
+				}
+				while(ReadKey()!=0xff);
+			}
+		}
+    }
+}
+************************************************************************************************/

8e3c6f256b19fa954471a8edab7f6727a947f33b
MrSu18 <3051605203@qq.com>
2023-02-24 18:50:55 +0800

    封装一下数码管的函数

--- software/libraries/sushizhou_device/su_digital_tube.h
+++ software/libraries/sushizhou_device/su_digital_tube.h
@@ -7,5 +7,8 @@
 #define ENABLE_DIGITALTUBE_WEIXUAN_LATCH P2=(P2&0X1F)|0xC0 //使能数码管段选锁存器Y6C
 
 extern uint8_t code t_display[];
+extern uint8_t code T_COM[];
+
+void DigitalTubeDisplay(uint8_t com,uint8_t num);//数码管显示
 
 #endif
--- software/user/main.c
+++ software/user/main.c
@@ -7,58 +7,62 @@
 
 void main(void)
 {
+	int i=0;
     ENABLE_LED_LATCH;
 	while (1)
     {
-		
+		for(i=1;i<8;i++)
+		{
+			DigitalTubeDisplay(i,9-i);
+		}
 		//*******第一位显示1*************
-		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;//Y6C有效，P0控制显示哪一位
-		P0=0x01;          //P0=0000 0001，选通第一位（最左边一位）
-		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;//Y7C有效，P0控制显示哪一个数字
-		P0=~t_display[1];	//给P0输入数字1的段码，让数码管显示1	
-		Delay2ms();
-		//*******第二位显示2*************	
-		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;	//Y6C有效
-		P0=0x02;          //P0=0000 0010，选通第二位
-		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;	//Y7C有效
-		P0=~t_display[2];	//给P0输入数字2的段码，让数码管显示2		
-		Delay2ms();
-		//*******第三位显示3*************		
-		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;	//Y6C有效
-		P0=0x04;					//P0=0000 0100，选通第三位
-		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;	//Y7C有效
-		P0=~t_display[3];	//给P0输入数字3的段码，让数码管显示3
-		Delay2ms();
-		//*******第四位显示4*************		
-		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
-		P0=0x08;					//P0=0000 1000，选通第四位
-		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
-		P0=~t_display[4];	//数码管显示4
-		Delay2ms();
-		//*******第五位显示*************		
-		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
-		P0=0x10;					//P0=0001 0000，选通第五位
-		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
-		P0=~t_display[5];	//数码管显示5
-		Delay2ms();
-		//*******第六位显示6*************		
-		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
-		P0=0x20;					//P0=0010 0000，选通第六位
-		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
-		P0=~t_display[6];	//数码管显示6	
-		Delay2ms();
-		//*******第七位显示7*************		
-		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
-		P0=0x40;					//P0=0100 0000，选通第七位
-		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
-		P0=~t_display[7];	//数码管显示7	
-		Delay2ms();
-		//*******第八位显示8*************		
-		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
-		P0=0x80;					//P0=1000 0000，选通第八位
-		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
-		P0=~t_display[8];	//数码管显示8	
-		Delay2ms();	
+//		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;//Y6C有效，P0控制显示哪一位
+//		P0=T_COM[0];          //P0=0000 0001，选通第一位（最左边一位）
+//		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;//Y7C有效，P0控制显示哪一个数字
+//		P0=~t_display[1];	//给P0输入数字1的段码，让数码管显示1	
+//		Delay2ms();
+//		//*******第二位显示2*************	
+//		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;	//Y6C有效
+//		P0=T_COM[1];          //P0=0000 0010，选通第二位
+//		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;	//Y7C有效
+//		P0=~t_display[2];	//给P0输入数字2的段码，让数码管显示2		
+//		Delay2ms();
+//		//*******第三位显示3*************		
+//		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;	//Y6C有效
+//		P0=0x04;					//P0=0000 0100，选通第三位
+//		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;	//Y7C有效
+//		P0=~t_display[3];	//给P0输入数字3的段码，让数码管显示3
+//		Delay2ms();
+//		//*******第四位显示4*************		
+//		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
+//		P0=0x08;					//P0=0000 1000，选通第四位
+//		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
+//		P0=~t_display[4];	//数码管显示4
+//		Delay2ms();
+//		//*******第五位显示*************		
+//		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
+//		P0=0x10;					//P0=0001 0000，选通第五位
+//		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
+//		P0=~t_display[5];	//数码管显示5
+//		Delay2ms();
+//		//*******第六位显示6*************		
+//		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
+//		P0=0x20;					//P0=0010 0000，选通第六位
+//		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
+//		P0=~t_display[6];	//数码管显示6	
+//		Delay2ms();
+//		//*******第七位显示7*************		
+//		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
+//		P0=0x40;					//P0=0100 0000，选通第七位
+//		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
+//		P0=~t_display[7];	//数码管显示7	
+//		Delay2ms();
+//		//*******第八位显示8*************		
+//		ENABLE_DIGITALTUBE_WEIXUAN_LATCH;
+//		P0=0x80;					//P0=1000 0000，选通第八位
+//		ENABLE_DIGITALTUBE_DUANGXUAN_LATCH;
+//		P0=~t_display[8];	//数码管显示8	
+//		Delay2ms();	
     }
 }
 

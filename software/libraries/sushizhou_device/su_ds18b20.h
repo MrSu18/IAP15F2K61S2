9a6ce900b5608750f3310c4f0a291a2c42edf253
MrSu18 <3051605203@qq.com>
2023-03-24 23:19:47 +0800

    温度传感器DS18B02用实验室拷回来的驱动文件，注意这次的驱动里面本来就是适配1T单片机的所以不需要乘10倍

--- software/libraries/sushizhou_device/su_ds18b20.h
+++ software/libraries/sushizhou_device/su_ds18b20.h
@@ -2,6 +2,6 @@
 #define __ONEWIRE_H
 
 unsigned char rd_temperature(void);
-char Read_DS18B20_temp();
+float Read_DS18B20_temp(void);
 
 #endif
--- software/user/main.c
+++ software/user/main.c
@@ -1,14 +1,21 @@
 #include "su_S12.h"
 #include "su_usart.h"
 #include "delay.h"
+#include "su_ds18b20.h"
 
 void main(void)
 {
+	float temperature=0;
+	char temp=0;
+	
 	DeviceInit();//设备初始化
 	UartInit();//串口初始化用于调试
+	
 	while(1)
 	{
-		printf("usart test\r\n");
+		temp=rd_temperature();
+//		temperature=Read_DS18B20_temp();
+		printf("%bu\r\n",temp);
 		Delay500ms();
 	}
 }

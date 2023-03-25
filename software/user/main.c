478f8b64ecc48ac6001a55e11cd2c77d57b03a3b
MrSu18 <3051605203@qq.com>
2023-03-25 19:50:30 +0800

    蜂鸣器、继电器

--- software/user/main.c
+++ software/user/main.c
@@ -4,15 +4,21 @@
 #include "su_ds18b20.h"
 #include "su_key.h"
 
+#include "su_latch.h"
+#include <stc15f2k60s2.h>
+
 void main(void)
 {	
-	unsigned char temp=0;
+	unsigned char temp=0,status=0;
 	DeviceInit();//设备初始化
 	UartInit();//串口初始化用于调试
 	
+	Select_Latch(5);
+	
 	while(1)
 	{
-		DisplaySetParameter();
+		P06=0;//继电器关
+		P04=0;//蜂鸣器关
 	}
 }
 

851b013a43b2238ef03ea41062e83bd184ff9186
MrSu18 <3051605203@qq.com>
2023-05-03 17:12:46 +0800

    修改NE555的数据类型的BUG

--- software/libraries/sushizhou_device/su_ne555.h
+++ software/libraries/sushizhou_device/su_ne555.h
@@ -3,7 +3,7 @@
 
 #include "su_common_typedef.h"
 
-extern uint8_t frequency;//频率
+extern uint16_t frequency;//频率
 
 //必须用定时器1作为定时器，因为板子上的P34才是拿来测量频率的引脚所以计数器要用定时器0
 void Ne555TimInit(void);//NE555定时器初始化
--- software/libraries/sushizhou_drive/timer.c
+++ software/libraries/sushizhou_drive/timer.c
@@ -9,20 +9,6 @@
 #include "su_digital_tube.h"
 #include "su_ne555.h"
 
-void Timer0Init(void)		//1毫秒@12.000MHz
-{
-	AUXR |= 0x80;			//定时器时钟1T模式
-	TMOD &= 0xF0;			//设置定时器模式
-	TL0 = 0x20;				//设置定时初始值
-	TH0 = 0xD1;				//设置定时初始值
-	TF0 = 0;				//清除TF0标志
-	TR0 = 1;				//定时器0开始计时
-	
-	/*上述的代码是ISP工具生成的定时器的模式初值设置代码，如果要开启中断则要自己写下面两句，开启中断*/
-	
-	ET0=1;//开启定时器0中断
-}
-
 void Time0_Sever() interrupt 1	//定时器0中断回调函数
 {
 //	S13TimeServer();

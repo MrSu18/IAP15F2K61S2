e7c43b2746d1b55740f08f65f068cf715664f7b8
MrSu18 <3051605203@qq.com>
2023-05-10 20:17:04 +0800

    测试新建分支

--- software/user/main.c
+++ software/user/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "su_latch.h"//锁存器
 #include "su_led.h"//LED的结构体
+#include "su_ne555.h"
 
 void main(void)
 {	
@@ -9,6 +10,11 @@ void main(void)
 	Select_Latch(5);P04=0;P06=0;
 	//初始化led全为暗
 	memset(&led_object,0xff,1);Select_Latch(4);P0=*(uint8_t*)&led_object;
+	//NE555初始化
+	Ne555TimInit();//NE555定时器初始化
+	Ne555CountInit();//NE555记数器初始化
+	//开启总中断EA
+	EA=1;
 	while (1)
 	{
 		;
